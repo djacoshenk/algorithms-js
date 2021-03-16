8e5b5ee9780f4714869ad05cfefa237ce1355c24
Danny Jacoshenk <hello@dannyjaco.me>
2021-03-15 22:34:28 -0700

    Refactor stack with array algo

--- src/stack/array-stack.ts
+++ src/stack/array-stack.ts
@@ -8,17 +8,17 @@ Space Complexity: ​O(1) - no extra space used apart from the array.
 */
 
 export class ArrayStack {
-  a: number[];
+  arr: number[];
   s1: number;
   s2: number;
 
   constructor(arraySize: number) {
-    this.a = new Array(arraySize);
+    this.arr = new Array(arraySize);
     this.s1 = 0;
-    this.s2 = this.a.length - 1;
+    this.s2 = this.arr.length - 1;
   }
 
-  push(stackNumber: number, item: number) {
+  push(stackNumber: number, data: number) {
     if (stackNumber !== 1 && stackNumber !== 2) {
       throw new Error('Invalid stack number');
     }
@@ -28,9 +28,9 @@ export class ArrayStack {
     }
 
     if (stackNumber === 1) {
-      this.a[this.s1++] = item;
+      this.arr[this.s1++] = data;
     } else {
-      this.a[this.s2--] = item;
+      this.arr[this.s2--] = data;
     }
   }
 
@@ -40,11 +40,30 @@ export class ArrayStack {
     }
 
     if (stackNumber === 1 && this.s1 > 0) {
-      return this.a[this.s1--];
-    } else if (stackNumber === 2 && this.s2 < this.a.length - 1) {
-      return this.a[this.s2++];
+      return this.arr[--this.s1];
+    } else if (stackNumber === 2 && this.s2 < this.arr.length - 1) {
+      return this.arr[++this.s2];
     } else {
       throw new Error('Stack empty');
     }
   }
 }
+
+const ARRAY_STACK = new ArrayStack(6);
+
+console.log(ARRAY_STACK);
+
+ARRAY_STACK.push(1, 2);
+ARRAY_STACK.push(1, 4);
+ARRAY_STACK.push(1, 6);
+ARRAY_STACK.push(2, 3);
+ARRAY_STACK.push(2, 5);
+ARRAY_STACK.push(2, 7);
+
+console.log(ARRAY_STACK);
+
+console.log(ARRAY_STACK.pop(1));
+
+ARRAY_STACK.push(1, 8);
+
+console.log(ARRAY_STACK);
