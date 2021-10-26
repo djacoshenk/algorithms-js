56589a1cf81129d18512f40214e9606c8151bc95
Danny Jacoshenk <hello@dannyjaco.me>
2021-10-26 12:37:51 -0700

    Add console logs for fibonacci fn

--- src/recursion-and-backtracking/fibonacci.ts
+++ src/recursion-and-backtracking/fibonacci.ts
@@ -12,3 +12,12 @@ function fibonacci(n: number): number {
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
+console.log(fibonacci(6)); // 8
+console.log(fibonacci(7)); // 13
+console.log(fibonacci(8)); // 21
